c524bc2d6e680f8e67d81ddf500960424e8b8e27
marieooq <otakimarie@gmail.com>
2024-04-04 20:32:08 +0700

    created form

---
+++ app/components/form.tsx
@@ -0,0 +1,68 @@
+import React, { useState } from "react";
+import { SubmitHandler, useForm } from "react-hook-form";
+
+type FormValues = { email: string; password: string };
+
+const Form = () => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<FormValues>();
+
+  const onSubmit: SubmitHandler<FormValues> = (data) => {
+    console.log({ data });
+  };
+
+  return (
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <div>
+        <label htmlFor="email">Email:</label>
+        <input
+          {...register("email", {
+            required: "Email is required",
+            validate: (value: string) => {
+              if (!value.includes("@")) {
+                return "Email must include @.";
+              } else {
+                return true;
+              }
+            },
+          })}
+          type="email"
+          id="email"
+          className="border-solid border-2 border-indigo-600"
+        />
+        {errors.email && (
+          <span className="text-red-500">{errors.email.message}</span>
+        )}
+      </div>
+      <div>
+        <label htmlFor="password">Password:</label>
+        <input
+          {...register("password", {
+            required: "Password is required",
+            minLength: {
+              value: 8,
+              message: "Password must be at least 8 characters",
+            },
+          })}
+          type="password"
+          id="password"
+          className="border-solid border-2 border-indigo-600"
+        />
+        {errors.password && (
+          <span className="text-red-500">{errors.password.message}</span>
+        )}
+      </div>
+      <button
+        type="submit"
+        className="text-white border-solid border-2 border-indigo-600 bg-indigo-600 p-2 rounded-md mt-2"
+      >
+        Submit
+      </button>
+    </form>
+  );
+};
+
+export default Form;
